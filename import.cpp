0a63d3fe4b58add1a1e04951a3b275379dd20a81
Hasnain2779 <bsse1427@iit.du.ac.bd>
2023-11-11 00:22:51 +0600

    import as create

---
+++ import.cpp
@@ -0,0 +1,63 @@
+void handleImportQuery(string query, TableList *table){
+    vector<string> queryTokens;
+    string data;
+    fstream file;
+    query = checkSemiColon(query);
+    if (!checkImportQuery(query)){
+        cout << "Something wrong with your query. Please check the syntax and try again." << endl;
+        return;
+    }
+    queryTokens = tokenizeString(query);
+
+    if(queryTokens.size()==1){
+        cout << "Incomplete command. Try again." << endl;
+        return;
+    }
+    if(queryTokens[1].find(".csv", 0) == string::npos){
+        cout << "Please enter a .csv file." << endl;
+        return;
+    }
+    file.open(queryTokens[1], ios::in);
+    if(!file){
+        cout << "File not found. Try again." << endl;
+        return;
+    }
+    // extra call for the column names in the csv file
+    getline(file, data);
+
+    string filename = (queryTokens[1].substr(0, queryTokens[1].length() - 3)).append("db");
+    int offset = 0;
+
+    IndexList id = IndexList(INT);
+    IndexList username = IndexList(STRING);
+    IndexList email = IndexList(STRING);
+    IndexList password = IndexList(STRING);
+
+    if(file.eof()){
+        cout << "File is empty. Please import a file with data." << endl;
+        return;
+    }
+    do{
+        getline(file, data);
+        vector<string> row = tokenizeString(data,",");
+        TableRecord record = TableRecord(stoi(row[0]), row[1], row[2], row[3]);
+        record.writeToFile(filename);
+        // record.testRecord();
+        (*table).insertRecord(&record);
+        id.insert(row[0],offset);
+        username.insert(row[1],offset);
+        email.insert(row[2],offset);
+        password.insert(row[3],offset);
+
+        offset += record.getLength();
+
+    }while(!file.eof());
+
+    id.writeToFile("../temp/id.ndx");
+    username.writeToFile("../temp/username.ndx");
+    email.writeToFile("../temp/email.ndx");
+    password.writeToFile("../temp/password.ndx");
+    file.close();
+
+    cout << "Import Success !!!" << endl;
+}
---
+++ select.cpp
@@ -0,0 +1,442 @@
+
+void printEqualtoElement(string element, string columnName, string filename){
+    // Print all the columns when the values are equal
+    // Select * from <table name> where <column name> = <element>
+    int offset = 0;
+    while(true){
+        try{
+            TableRecord record = TableRecord(filename,offset);
+            if (record.getPasswordLength() >= 15 ){
+                break;
+            }
+            offset += record.getLength();
+            if(!record.isDeleted()){
+                if(columnName == "id" && record.getId() != stoi(element)){
+                    continue;
+                }
+                else if(columnName == "username" && record.getUsername() != element){
+                    continue;
+                }
+                else if(columnName == "password" && record.getPassword() != element){
+                    continue;
+                }
+                else if(columnName == "email" && record.getEmail() != element){
+                    continue;
+                }
+                record.testRecord();
+            }
+        }catch(string err){
+            break;
+        }
+    }
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+void printNotEqualtoElement(string element, string columnName, string filename){
+    // Print all the columns where the values are not equal
+    // Select * from <table name> where <column name> != <element>
+    int offset = 0;
+    while(true){
+        try{
+            TableRecord record = TableRecord(filename,offset);
+            if (record.getPasswordLength() >= 15 ){
+                break;
+            }
+            offset += record.getLength();
+            if(!record.isDeleted()){
+                if(columnName == "id" && record.getId() == stoi(element)){
+                    continue;
+                }
+                else if(columnName == "username" && record.getUsername() == element){
+                    continue;
+                }
+                else if(columnName == "password" && record.getPassword() == element){
+                    continue;
+                }
+                else if(columnName == "email" && record.getEmail() == element){
+                    continue;
+                }
+                record.testRecord();
+            }
+        }catch(string err){
+            break;
+        }
+    }
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+void printLessThanElement(string element, string columnName, string filename){
+    // Print all the columns when the values are less than
+    // Select * from <table name> where <column name> < <element>
+    int offset = 0;
+    while(true){
+        try{
+            TableRecord record = TableRecord(filename,offset);
+            if (record.getPasswordLength() >= 15 ){
+                break;
+            }
+            offset += record.getLength();
+            if(!record.isDeleted()){
+                if(columnName == "id" && record.getId() >= stoi(element)){
+                    continue;
+                }
+                else if(columnName == "username" && record.getUsername() >= element){
+                    continue;
+                }
+                else if(columnName == "password" && record.getPassword() >= element){
+                    continue;
+                }
+                else if(columnName == "email" && record.getEmail() >= element){
+                    continue;
+                }
+                record.testRecord();
+            }
+        }catch(string err){
+            break;
+        }
+    }
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+void printGreaterThanElement(string element, string columnName, string filename){
+    // Print all the columns when the values are greater than
+    // Select * from <table name> where <column name> > <element>
+    int offset = 0;
+    while(true){
+        try{
+            TableRecord record = TableRecord(filename,offset);
+            if (record.getPasswordLength() >= 15 ){
+                break;
+            }
+            offset += record.getLength();
+            if(!record.isDeleted()){
+                if(columnName == "id" && record.getId() <= stoi(element)){
+                    continue;
+                }
+                else if(columnName == "username" && record.getUsername() <= element){
+                    continue;
+                }
+                else if(columnName == "password" && record.getPassword() <= element){
+                    continue;
+                }
+                else if(columnName == "email" && record.getEmail() <= element){
+                    continue;
+                }
+                record.testRecord();
+            }
+        }catch(string err){
+            break;
+        }
+    }
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+void printLessThanOrEqualToElement(string element, string columnName, string filename){
+    // Print all the columns when the values are less than or equal
+    // Select * from <table name> where <column name> <= <element>
+    int offset = 0;
+    while(true){
+        try{
+            TableRecord record = TableRecord(filename,offset);
+            if (record.getPasswordLength() >= 15 ){
+                break;
+            }
+            offset += record.getLength();
+            if(!record.isDeleted()){
+                if(columnName == "id" && record.getId() > stoi(element)){
+                    continue;
+                }
+                else if(columnName == "username" && record.getUsername() > element){
+                    continue;
+                }
+                else if(columnName == "password" && record.getPassword() > element){
+                    continue;
+                }
+                else if(columnName == "email" && record.getEmail() > element){
+                    continue;
+                }
+                record.testRecord();
+            }
+        }catch(string err){
+            break;
+        }
+    }
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+void printGreaterThanOrEqualToElement(string element, string columnName, string filename){
+    // Print all the columns when the values are greater than or equal to
+    // Select * from <table name> where <column name> >= <element>
+    int offset = 0;
+    while(true){
+        try{
+            TableRecord record = TableRecord(filename,offset);
+            if (record.getPasswordLength() >= 15 ){
+                break;
+            }
+            offset += record.getLength();
+            if(!record.isDeleted()){
+                if(columnName == "id" && record.getId() < stoi(element)){
+                    continue;
+                }
+                else if(columnName == "username" && record.getUsername() < element){
+                    continue;
+                }
+                else if(columnName == "password" && record.getPassword() < element){
+                    continue;
+                }
+                else if(columnName == "email" && record.getEmail() < element){
+                    continue;
+                }
+                record.testRecord();
+            }
+        }catch(string err){
+            break;
+        }
+    }
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+void printAllElements(string filename){
+    // Print all the rows
+    // Select * from <table name>
+    int offset = 0;
+    while(true){
+        try{
+            TableRecord record = TableRecord(filename,offset);
+            if (record.getPasswordLength() >= 15 ){
+                break;
+            }
+            if(!record.isDeleted()){
+                record.testRecord();
+            }
+            offset += record.getLength();
+        }catch(string err){
+            break;
+        }
+    }
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+void printAllColumnsHeader(){
+    // Print all the column names in a tabluar form
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    cout << "|";
+    cout << setw(6/2) << setfill(' ') << "id" << setw(6/3) << "|";
+    cout << setw(20/2) << setfill(' ') << "username" << setw(20/3) << "|";
+    cout << setw((40/2)-1) << setfill(' ') << "email" << setw((40/3)) << "|";
+    cout << setw(20/2) << setfill(' ') << "password" << setw(20/3) << "|";
+    cout << endl;
+    cout << "+----+---------------+-------------------------------+---------------+";
+    cout << endl; 
+    return;
+}
+
+
+void handleSelectQuery(string query){
+    vector<string> queryTokens;
+    fstream file;
+    query = checkSemiColon(query);
+    if (!checkSelectQuery(query)){
+        cout << "Something wrong with your query. Please check the syntax and try again." << endl;
+        return;
+    }
+    queryTokens = tokenizeString(query);
+
+    if(queryTokens.size() < 4){
+        cout << "Incomplete command. Try again." << endl;
+        return;
+    }
+    string filename = queryTokens[3].append(".db");
+
+    if(access( (&filename)->c_str(), F_OK ) == -1){
+        cout << "Error: database hasn't been initialized. Please initialize the database using the import query." << endl;
+        return;
+    }
+    // Where keyword is not present
+    if(queryTokens.size() == 4){
+        // printing all the records
+        if (queryTokens[1] == "*"){
+            printAllColumnsHeader();
+            printAllElements(filename);
+        }
+    }
+    // WHERE keyword is present
+    else{
+        if (queryTokens[1] == "*"){
+            // WHERE clause used on only one column
+            if(queryTokens.size() < 9){
+                if (queryTokens[5] == "id"){
+                    // Where clause on column id
+                    if (queryTokens[6] == "="){
+                        // operation : equal to
+                        printAllColumnsHeader();
+                        printEqualtoElement(queryTokens[7],"id",filename);
+                    }
+                    else if (queryTokens[6] == "!="){
+                        // operation : not equal to
+                        printAllColumnsHeader();
+                        printNotEqualtoElement(queryTokens[7],"id",filename);
+                    }
+                    else if (queryTokens[6] == "<"){
+                        // operation : less than
+                        printAllColumnsHeader();
+                        printLessThanElement(queryTokens[7],"id",filename);
+                    }
+                    else if (queryTokens[6] == ">"){
+                        // operation : greater than
+                        printAllColumnsHeader();
+                        printGreaterThanElement(queryTokens[7],"id",filename);
+                    }
+                    else if (queryTokens[6] == "<="){
+                        // operation : less than or equal to
+                        printAllColumnsHeader();
+                        printLessThanOrEqualToElement(queryTokens[7],"id",filename);
+                    }
+                    else if (queryTokens[6] == ">="){
+                        // operation : greater than or equal to
+                        printAllColumnsHeader();
+                        printGreaterThanOrEqualToElement(queryTokens[7],"id",filename);
+                    }
+                    else{
+                        cout << "Error: Invalid operation. Expected operations : =, !=, >, <, >=, <=" << endl;
+                        return;
+                    }
+                }
+                else if (queryTokens[5] == "username"){
+                    // Where clause on column username
+                    if (queryTokens[6] == "="){
+                        // operation : equal to
+                        printAllColumnsHeader();
+                        printEqualtoElement(queryTokens[7],"username",filename);
+                    }
+                    else if (queryTokens[6] == "!="){
+                        // operation : not equal to
+                        printAllColumnsHeader();
+                        printNotEqualtoElement(queryTokens[7],"username",filename);
+                    }
+                    else if (queryTokens[6] == "<"){
+                        // operation : less than
+                        printAllColumnsHeader();
+                        printLessThanElement(queryTokens[7],"username",filename);
+                    }
+                    else if (queryTokens[6] == ">"){
+                        // operation : greater than
+                        printAllColumnsHeader();
+                        printGreaterThanElement(queryTokens[7],"username",filename);
+                    }
+                    else if (queryTokens[6] == "<="){
+                        // operation : less than or equal to
+                        printAllColumnsHeader();
+                        printLessThanOrEqualToElement(queryTokens[7],"username",filename);
+                    }
+                    else if (queryTokens[6] == ">="){
+                        // operation : greater than or equal to
+                        printAllColumnsHeader();
+                        printGreaterThanOrEqualToElement(queryTokens[7],"username",filename);
+                    }
+                    else{
+                        cout << "Error: Invalid operation. Expected operations : =, !=, >, <, >=, <=" << endl;
+                        return;
+                    }
+                }
+                else if (queryTokens[5] == "email"){
+                    // Where clause on column email
+                    if (queryTokens[6] == "="){
+                        // operation : equal to
+                        printAllColumnsHeader();
+                        printEqualtoElement(queryTokens[7],"email",filename);
+                    }
+                    else if (queryTokens[6] == "!="){
+                        // operation : not equal to
+                        printAllColumnsHeader();
+                        printNotEqualtoElement(queryTokens[7],"email",filename);
+                    }
+                    else if (queryTokens[6] == "<"){
+                        // operation : less than
+                        printAllColumnsHeader();
+                        printLessThanElement(queryTokens[7],"email",filename);
+                    }
+                    else if (queryTokens[6] == ">"){
+                        // operation : greater than
+                        printAllColumnsHeader();
+                        printGreaterThanElement(queryTokens[7],"email",filename);
+                    }
+                    else if (queryTokens[6] == "<="){
+                        // operation : less than or equal to
+                        printAllColumnsHeader();
+                        printLessThanOrEqualToElement(queryTokens[7],"email",filename);
+                    }
+                    else if (queryTokens[6] == ">="){
+                        // operation : greater than or equal to
+                        printAllColumnsHeader();
+                        printGreaterThanOrEqualToElement(queryTokens[7],"email",filename);
+                    }
+                    else{
+                        cout << "Error: Invalid operation. Expected operations : =, !=, >, <, >=, <=" << endl;
+                        return;
+                    }
+                }
+                else if (queryTokens[5] == "password"){
+                    // Where clause on column password
+                    if (queryTokens[6] == "="){
+                        // operation : equal to
+                        printAllColumnsHeader();
+                        printEqualtoElement(queryTokens[7],"password",filename);
+                    }
+                    else if (queryTokens[6] == "!="){
+                        // operation : not equal to
+                        printAllColumnsHeader();
+                        printNotEqualtoElement(queryTokens[7],"password",filename);
+                    }
+                    else if (queryTokens[6] == "<"){
+                        // operation : less than
+                        printAllColumnsHeader();
+                        printLessThanElement(queryTokens[7],"password",filename);
+                    }
+                    else if (queryTokens[6] == ">"){
+                        // operation : greater than
+                        printAllColumnsHeader();
+                        printGreaterThanElement(queryTokens[7],"password",filename);
+                    }
+                    else if (queryTokens[6] == "<="){
+                        // operation : less than or equal to
+                        printAllColumnsHeader();
+                        printLessThanOrEqualToElement(queryTokens[7],"password",filename);
+                    }
+                    else if (queryTokens[6] == ">="){
+                        // operation : greater than or equal to
+                        printAllColumnsHeader();
+                        printGreaterThanOrEqualToElement(queryTokens[7],"password",filename);
+                    }
+                    else{
+                        cout << "Error: Invalid operation. Expected operations : =, !=, >, <, >=, <=" << endl;
+                        return;
+                    }
+                }
+                else{
+                    cout << "Error : Column not found. Please try again." << endl;
+                    return;
+                }
+
+            }
+        }
+    }
+
+
+}
